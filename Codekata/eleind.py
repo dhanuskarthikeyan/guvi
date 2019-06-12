b3463381ee76187540ce277baada57f391118e03
dhanuskarthikeyan <46639174+dhanuskarthikeyan@users.noreply.github.com>
2019-06-12 15:30:35 +0530

    Update eleind.py

--- Codekata/eleind.py
+++ Codekata/eleind.py
@@ -1,5 +1,6 @@
-a1=int(input())
-p=list(map(int,input().split()))
-for i in range(0,len(p)):
-  print(p[i],i)
-  print()
+a=int(input())
+ba=[]
+for x in raw_input().split():
+  ba.append(int(x))
+for i in range(0,len(ba)):
+  print ba[i],i
