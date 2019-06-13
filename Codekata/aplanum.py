10575c813bcf45f1aef905fd9b699c83e72fa6f3
dhanuskarthikeyan <46639174+dhanuskarthikeyan@users.noreply.github.com>
2019-06-13 12:18:57 +0530

    Create aplanum.py

---
+++ Codekata/aplanum.py
@@ -0,0 +1,11 @@
+n=raw_input()
+co=0
+for i in range(0,len(n)):
+  if n[i].isalpha() or n[i].isdigit() or n[i]==' ' or n[i]==".":
+    continue
+  else:
+    break
+if i==len(n)-1 :
+  print "yes"
+else :
+  print "no"
