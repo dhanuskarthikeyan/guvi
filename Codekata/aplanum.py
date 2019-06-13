a3c24ae014f79880c0cb595fec3e186364a0355a
dhanuskarthikeyan <46639174+dhanuskarthikeyan@users.noreply.github.com>
2019-06-13 12:23:30 +0530

    Update aplanum.py

--- Codekata/aplanum.py
+++ Codekata/aplanum.py
@@ -1,11 +1,14 @@
 n=raw_input()
 co=0
+ko=0
 for i in range(0,len(n)):
-  if n[i].isalpha() or n[i].isdigit() or n[i]==' ' or n[i]==".":
-    continue
+  if n[i].isalpha():
+    co=co+1
+  elif n[i].isdigit():
+    ko=ko+1  
   else:
-    break
-if i==len(n)-1 :
-  print "Yes"
+    continue
+if ko>=1 and co>=1:
+  print "yes"
 else :
-  print "No"
+  print "no"
