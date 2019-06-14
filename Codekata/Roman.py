a7815b9df82e1793a75611dc187d7bc8fe8964e7
dhanuskarthikeyan <46639174+dhanuskarthikeyan@users.noreply.github.com>
2019-06-14 16:58:56 +0530

    Create Roman.py

---
+++ Codekata/Roman.py
@@ -0,0 +1,5 @@
+k =(raw_input())
+li=["Zero","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if k==li[i]:
+    print i
